9516ebf2ec1737517ceccbed5771c84c4cfb2178
Erick12354 <erickmadrigalduran@gmail.com>
2026-04-22 14:25:07 -0600

    quiz3

---
+++ quizes/quiz003/quiz2_Erick_Madrigal.cpp
@@ -0,0 +1,63 @@
+
+// Ejercicio: Vagones del tren
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Define la lista de vagones 
+int vagones[] = {};
+int N;
+for (int i = 0; i < N; i++){
+    vagones = vagones + 0;
+    return vagones;
+}
+
+//Añadir carga
+void cargar(int p,int v){
+    int vagones;
+        for (int p ){
+            vagones[i] = int v;
+            return vagones;
+}
+}
+
+//Consultar 
+void consultar(int i = 0; i < p; i++){
+    int vagones;
+    int num_consulta;+
+    num_consulta = vagones[p];
+    return num_consulta;
+}
+
+
+int main(){
+int N; //Cantidad de vagones 
+int Q; // Cantidad de operaciónes
+
+cin >> N;
+cin >> Q;
+
+
+// Realizar las operaciones
+for (i int Q){
+    int opcion;
+    cin >> opcion;
+
+    if (opcion == A){
+        int p; //Cantidad de vagones 
+        int v; // Carga sumada a cada valor 
+
+        cin >> p;
+        cin >> v;
+        cargar(p,v);
+    }
+
+    else if (opcion == C){
+        int p; // Posición del vagón
+       
+        cin >> p;
+        consultar(p);
+        cout << consultar << '\n';   
+} 
+}
+}
